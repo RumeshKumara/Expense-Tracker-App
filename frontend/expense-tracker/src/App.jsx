a3824a48d6ec26bf235a2b4d43d0a82112f9f2b9
rumesh <rumeshk066@gmail.com>
2025-03-03 21:11:10 +0530

    first commit

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className=" text-pink-600 text-1xl">App</div>
+    </>
+  );
+}
+
+export default App;
